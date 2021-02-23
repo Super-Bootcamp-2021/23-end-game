d7bf3f1fe50abc1a117c3a0796e1d0be69d0a4c8
Muhammad Hasan <hasan@sirus.dev>
2021-02-23 18:35:07 +0700

    add spec to performance refresh

--- webapp/test/specs/performance.ts
+++ webapp/test/specs/performance.ts
@@ -1,17 +1,42 @@
 describe('performance page', () => {
   it('sould display performance information', () => {
-    cy.intercept('/summary', {
-      total_task: 5,
-      task_done: 2,
-      task_cancelled: 3,
-      total_worker: 3,
-    }).as('getSummary');
+    cy.intercept('/summary', { fixture: 'performance-summary' }).as(
+      'getSummary'
+    );
     cy.visit('/performance.html');
     cy.wait('@getSummary');
     cy.get('#tasks').should('have.text', '5');
     cy.get('#task-done').should('have.text', '2');
     cy.get('#task-canceled').should('have.text', '3');
     cy.get('#workers').should('have.text', '3');
+  });
+
+  it('should be able to refresh information', () => {
+    let i = 0;
+    cy.intercept('/summary', (req) => {
+      if (i > 0) {
+        req.reply({
+          total_task: 21,
+          task_done: 11,
+          task_cancelled: 10,
+          total_worker: 9,
+        });
+      } else {
+        req.reply({ fixture: 'performance-summary' });
+      }
+      req.alias = 'getSummary';
+      i++;
+    });
+    cy.visit('/performance.html');
+    cy.wait('@getSummary');
+    cy.get('#tasks').should('have.text', '5');
+    cy.get('#refresh').click();
+    cy.get('#loading-text').should('contain.text', 'memuat');
+    cy.wait('@getSummary');
+    cy.get('#tasks').should('have.text', '21');
+    cy.get('#task-done').should('have.text', '11');
+    cy.get('#task-canceled').should('have.text', '10');
+    cy.get('#workers').should('have.text', '9');
   });
 
   context('while loading summary', () => {
